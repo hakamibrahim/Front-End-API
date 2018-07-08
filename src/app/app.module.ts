a946cc5d69ba620f93557864f7f30c056ba686c1
h4km <hakamibrahim7@gmail.com>
2018-07-08 16:56:46 +0700

    add report pdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+
+
 
 @NgModule({
   declarations: [
@@ -14,6 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    PDFExportModule,
+    BrowserAnimationsModule,
   ],
   providers: [AppComponent],
   bootstrap: [AppComponent]
